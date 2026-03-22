e4184fadb7bf40efad6b24983758baf7a0c02002
kimds1110 <ds6951@naver.com>
2026-03-22 14:45:40 +0900

    260322 서버시작 10분 후 자동 실행에 라이브스트림인서트를 추가.

--- src/main/java/com/haconaka/demo/controller/api/MemberController.java
+++ src/main/java/com/haconaka/demo/controller/api/MemberController.java
@@ -5,10 +5,10 @@ import com.haconaka.demo.service.api.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +16,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class MemberController {
 
     private final MemberService memberService;
